e832f755ed8ef266ca57dd2285800b06cf0554e3
johnP-Li <847041172@qq.com>
2024-07-22 16:27:57 +0800

    Init commit

---
+++ datasets/download_ABIDEI.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please specify the directory where you would like to save the ABIDE I dataset:"
+read data_dir
+
+python3 -c "from nilearn import datasets; datasets.fetch_abide_pcp(data_dir='$data_dir')"
