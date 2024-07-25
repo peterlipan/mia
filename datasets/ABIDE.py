cc0b34363c6f290909c45b67be6546beab7847bc
LI Pan <r20user17@research20.saas.hku.hk>
2024-07-25 14:20:56 +0800

    frame loading

--- datasets/ABIDE.py
+++ datasets/ABIDE.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset
 
 
 class AbideFrameDataset(Dataset):
-    def __init__(self, csv, data_root, suffix='_func_preproc.nii.gz', task='DX', transforms=None, training=True):
+    def __init__(self, csv, data_root, suffix='_func_preproc.nii.gz', task='DX', transforms=None, mode='frames'):
         self.csv = csv
         self.filenames = csv['FILE_ID'].values
         self.labels = csv['DX_GROUP'].values
@@ -17,17 +17,24 @@ class AbideFrameDataset(Dataset):
         self.data_root = data_root
         self.transforms = transforms
         self.n_classes = len(np.unique(self.labels))
+        self.mode = mode
     
     def __len__(self):
         return len(self.labels)
     
     def __getitem__(self, idx):
         label = self.labels[idx]
-        file_path = os.path.join(self.data_root, self.filenames[idx] + self.suffix)
+        file_id = self.filenames[idx]
         frame_id = self.frame_id[idx]
-        # TODO: make the loading more efficient
-        frame = tio.ScalarImage(file_path)
-        frame.set_data(frame.data[frame_id][np.newaxis, ...])
+        frame = None
+        if self.mode == 'fMRI':
+            file_path = os.path.join(self.data_root, self.filenames[idx] + self.suffix)
+            # TODO: make the loading more efficient
+            frame = tio.ScalarImage(file_path)
+            frame.set_data(frame.data[frame_id][np.newaxis, ...])
+        elif self.mode == 'frames':
+            frame_path = os.path.join(self.data_root, file_id, f'frame_{frame_id}.nii.gz')
+            frame = tio.ScalarImage(frame_path)
         if self.transforms:
             frame = self.transforms(frame)
 
