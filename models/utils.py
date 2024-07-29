2a5e055e35ed145e72133e029f9acd63dc8ff7a4
johnP-Li <847041172@qq.com>
2024-07-29 16:39:55 +0800

    Init commit

--- models/utils.py
+++ models/utils.py
@@ -7,6 +7,16 @@ def get_classifier(n_features, n_classes):
     return nn.Linear(n_features, n_classes)
 
 
+def get_encoder(args):
+    if args.model == 'groupvit':
+        from .GroupViT import GroupViT
+        return GroupViT(img_size=args.image_size, patch_size=args.patch_size)
+    elif args.model == 'swin':
+        from models.Swin3D import SwinTransformer3d
+        return SwinTransformer3d(patch_size=args.patch_size)
+    else:
+        raise NotImplementedError(f"Model {args.model} not implemented")
+
 class LinearClassifier(nn.Module):
     def __init__(self, n_features, n_classes):
         super().__init__()
